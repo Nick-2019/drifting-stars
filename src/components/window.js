968d30f3293b33af1eebb7abcfc6bb0aa1982422
github username <github email address>
2019-10-11 01:58:37 -0500

    I can has map?

--- src/components/window.js
+++ src/components/window.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Message from './subComp/message'
 
+
+const arr = ["stromg", "string", "bing", "Linger"]
 export default class Window extends Component{
+    
     render(){
         return(
-            <div>
-                WINDOW DIV!
+            <div id="window">
+                <div>
+                    {arr.map(word => <Message words={word}></Message> )}
+                </div>
+                <br></br>
+                <button>Clear Thoughts</button>
             </div>
         )
     }
