3b4acefd5df00bba101c1fa7c0c1c3e1753a3de4
github username <github email address>
2019-10-19 13:44:56 -0500

    Store setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
+import allReducer from './reducers'
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
