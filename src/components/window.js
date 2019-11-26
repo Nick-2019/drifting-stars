4015ee5268806f373c2d6bb339ebee8caf7fa613
github username <github email address>
2019-11-26 12:36:04 -0600

    aaa

--- src/components/window.js
+++ src/components/window.js
@@ -10,14 +10,36 @@ import {add, clear} from '../actions'
 export default function Window(){
     const arr = useSelector(state => state.textBox.arr)
     const dispatch = useDispatch()
+
+    function wait(){document.getElementById("window").style.opacity = 0.1}
+
+    const buttonAction = () => {
+
+        // console.log("We did it, reddit!")
+        // setInterval(wait(0.5), 50000)
+        setTimeout(wait(), 9000)
+        console.log("we did it?")
         
+        // document.getElementById("window").style.opacity = 0.1
+
+
+        // dispatch(clear())
+
+    }
+        
+
+
         return(
+            <div>
             <div id="window">
                 <div>
                     {arr.map(word => <Message words={word}></Message> )}
                 </div>
                 <br></br>
-                <button onClick={() => dispatch(clear()) } >Clear Thoughts</button>
+                
+            </div>
+
+            <div><button onClick={() => buttonAction() } >Clear Thoughts</button></div>
             </div>
         )
     }
